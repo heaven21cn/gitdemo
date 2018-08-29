0a41f13feb94398f8ebb0fd9bdf362a319801209
heaven21cn <heaven21cn@163.com>
2018-08-29 11:30:21 +0800

    add user

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+测试
+ */
 @SpringBootApplication
 public class DemoApplication {
 
