ca6b192ec955281574aa78d42e0fa6b2e2893182
heaven21cn <heaven21cn@163.com>
2018-08-29 11:08:18 +0800

    add user

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,31 @@
+package main.java.com.example.demo.domain;
+
+public class User {
+    Integer id;
+    String name;
+    int age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
